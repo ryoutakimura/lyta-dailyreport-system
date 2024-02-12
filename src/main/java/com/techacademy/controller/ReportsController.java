8285859100278fe03e8ec68881a26889a45bd5a1
ryoutakimura <yellow11053@gmail.com>
2024-02-12 20:55:46 +0900

    日報一覧画面への遷移を実装

--- src/main/java/com/techacademy/controller/ReportsController.java
+++ src/main/java/com/techacademy/controller/ReportsController.java
@@ -7,21 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.techacademy.service.EmployeeService;
+import com.techacademy.service.ReportService;
 
 @Controller
 @RequestMapping("reports")
 public class ReportsController {
 
-    private final EmployeeService employeeService;
+    private final ReportService reportService;
 
     @Autowired
-    public ReportsController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+    public ReportsController(EmployeeService employeeService,ReportService reportService) {
+        this.reportService=reportService;
     }
 
     //日報一覧画面
     @GetMapping("")
     public String list(Model model) {
+
+
+        model.addAttribute("listSize", reportService.findAll().size());
+        model.addAttribute("reportList",reportService.findAll());
+
         return "reports/list";
     }
 
