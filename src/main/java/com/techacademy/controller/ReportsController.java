b873a3cea40d27847f39cc8afacd4d441922f441
ryoutakimura <yellow11053@gmail.com>
2024-02-13 23:45:18 +0900

    権限によって日報の取得件数を変える

--- src/main/java/com/techacademy/controller/ReportsController.java
+++ src/main/java/com/techacademy/controller/ReportsController.java
@@ -1,6 +1,7 @@
 package com.techacademy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.techacademy.service.EmployeeService;
 import com.techacademy.service.ReportService;
+import com.techacademy.service.UserDetail;
 
 @Controller
 @RequestMapping("reports")
@@ -22,11 +24,17 @@ public class ReportsController {
 
     //日報一覧画面
     @GetMapping("")
-    public String list(Model model) {
+    public String list(Model model,@AuthenticationPrincipal UserDetail userDetail) {
 
-
+        //ADMIN権限の場合すべての日報をVIEWに渡す
+        if(userDetail.getEmployee().getRole().toString() == "ADMIN") {
         model.addAttribute("listSize", reportService.findAll().size());
         model.addAttribute("reportList",reportService.findAll());
+        }else {
+        //GENERAL権限の場合自身の日報をVIEWに渡す
+        model.addAttribute("report",reportService.getReport(Integer.parseInt(userDetail.getEmployee().getCode())));
+        model.addAttribute("listSize", 1);
+        }
 
         return "reports/list";
     }
