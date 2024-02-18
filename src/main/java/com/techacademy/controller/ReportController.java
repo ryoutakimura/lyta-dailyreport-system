08f23769e2ca24665089e535175d0c984289d119
ryoutakimura <yellow11053@gmail.com>
2024-02-18 18:56:30 +0900

    日報一覧画面の表示の実装

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -4,21 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.entity.Report;
 import com.techacademy.service.EmployeeService;
 import com.techacademy.service.ReportService;
 import com.techacademy.service.UserDetail;
 
 @Controller
 @RequestMapping("reports")
-public class ReportsController {
+public class ReportController {
 
     private final ReportService reportService;
 
     @Autowired
-    public ReportsController(EmployeeService employeeService,ReportService reportService) {
+    public ReportController(EmployeeService employeeService,ReportService reportService) {
         this.reportService=reportService;
     }
 
@@ -38,5 +43,26 @@ public class ReportsController {
 
         return "reports/list";
     }
+
+    //日報新規登録画面
+    @GetMapping(value = "/add")
+    public String create(@ModelAttribute Report report, Model model,@AuthenticationPrincipal UserDetail userDetail) {
+        //ログインユーザーの情報をVIEWに渡す
+        model.addAttribute("user",reportService.getReport(Integer.parseInt(userDetail.getEmployee().getCode())).getEmployee());
+        return "reports/new";
+    }
+
+    //日報新規登録処理
+    @PostMapping(value ="/add")
+    public String add(@Validated Report report , BindingResult res, Model model,@AuthenticationPrincipal UserDetail userDetail) {
+        // 入力チェック
+        if (res.hasErrors()) {
+            System.out.println(res.getAllErrors());
+            return create(report,model,userDetail);
+        }
+
+        return "redirect:/reports";
+    }
+
 
 }
