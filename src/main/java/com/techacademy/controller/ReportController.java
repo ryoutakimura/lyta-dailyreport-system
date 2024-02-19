dc5c2806bc704ef009f5277f881ff4cad808258e
ryoutakimura <yellow11053@gmail.com>
2024-02-20 07:13:08 +0900

    日報新規登録画面の実装

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -1,6 +1,8 @@
 package com.techacademy.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.constants.ErrorMessage;
 import com.techacademy.entity.Report;
 import com.techacademy.service.EmployeeService;
 import com.techacademy.service.ReportService;
@@ -23,46 +27,65 @@ public class ReportController {
     private final ReportService reportService;
 
     @Autowired
-    public ReportController(EmployeeService employeeService,ReportService reportService) {
-        this.reportService=reportService;
+    public ReportController(EmployeeService employeeService, ReportService reportService) {
+        this.reportService = reportService;
     }
 
-    //日報一覧画面
+    // 日報一覧画面
     @GetMapping("")
-    public String list(Model model,@AuthenticationPrincipal UserDetail userDetail) {
+    public String list(Model model, @AuthenticationPrincipal UserDetail userDetail) {
 
-        //ADMIN権限の場合すべての日報をVIEWに渡す
-        if(userDetail.getEmployee().getRole().toString() == "ADMIN") {
-        model.addAttribute("listSize", reportService.findAll().size());
-        model.addAttribute("reportList",reportService.findAll());
-        }else {
-        //GENERAL権限の場合自身の日報をVIEWに渡す
-        model.addAttribute("report",reportService.getReport(Integer.parseInt(userDetail.getEmployee().getCode())));
-        model.addAttribute("listSize", 1);
+        model.addAttribute("user", userDetail);
+        // ADMIN権限の場合すべての日報をVIEWに渡す
+        if (userDetail.getEmployee().getRole().toString() == "ADMIN") {
+            model.addAttribute("listSize", reportService.findAll().size());
+            model.addAttribute("reportList", reportService.findAll());
+        } else {
+            // GENERAL権限の場合自身の日報をVIEWに渡す
+            model.addAttribute("listSize",
+                    reportService.findByUser(userDetail, (int) reportService.findAll().size()).size());
+            model.addAttribute("reportList",
+                    reportService.findByUser(userDetail, (int) reportService.findAll().size()));
         }
 
         return "reports/list";
     }
 
-    //日報新規登録画面
+    // 日報新規登録画面
     @GetMapping(value = "/add")
-    public String create(@ModelAttribute Report report, Model model,@AuthenticationPrincipal UserDetail userDetail) {
-        //ログインユーザーの情報をVIEWに渡す
-        model.addAttribute("user",reportService.getReport(Integer.parseInt(userDetail.getEmployee().getCode())).getEmployee());
+    public String create(@ModelAttribute Report report, Model model, @AuthenticationPrincipal UserDetail userDetail) {
+        // ログインユーザーの情報をVIEWに渡す
+        model.addAttribute("user",
+                reportService.findByCode(Integer.parseInt(userDetail.getEmployee().getCode())).getEmployee());
         return "reports/new";
     }
 
-    //日報新規登録処理
-    @PostMapping(value ="/add")
-    public String add(@Validated Report report , BindingResult res, Model model,@AuthenticationPrincipal UserDetail userDetail) {
+    // 日報新規登録処理
+    @PostMapping(value = "/add")
+    public String add(@Validated Report report, BindingResult res, Model model,
+            @AuthenticationPrincipal UserDetail userDetail) {
         // 入力チェック
         if (res.hasErrors()) {
-            System.out.println(res.getAllErrors());
-            return create(report,model,userDetail);
+            return create(report, model, userDetail);
+        }
+
+        // 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
+        // (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
+        try {
+            ErrorKinds result = reportService.save(report, userDetail);
+
+            if (ErrorMessage.contains(result)) {
+                model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+                return create(report, model, userDetail);
+            }
+
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
+                    ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
+            return create(report, model, userDetail);
         }
 
         return "redirect:/reports";
     }
-
 
 }
--- src/main/java/com/techacademy/service/ReportService.java
+++ src/main/java/com/techacademy/service/ReportService.java
@@ -1,12 +1,18 @@
 package com.techacademy.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.techacademy.constants.ErrorKinds;
 import com.techacademy.entity.Report;
 import com.techacademy.repository.ReportRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ReportService {
@@ -22,11 +28,52 @@ public class ReportService {
         return reportRepository.findAll();
     }
 
-    //日報をを１件検索
-    public Report getReport(Integer id) {
-        return reportRepository.findById(id).get();
+    // 日報1件を検索
+    public Report findByCode(Integer id) {
+        // findByIdで検索
+        Optional<Report> option = reportRepository.findById(id);
+        // 取得できなかった場合はnullを返す
+        Report report = option.orElse(null);
+        return report;
     }
 
+    // ログインユーザーの日報をすべて取得
+    public List<Report> findByUser(UserDetail userDetail, int size) {
+        List<Report> report = new ArrayList<>();
+        for (int i = 1; i <= size; i++) {
+            if (userDetail.getEmployee().getCode().equals(findByCode((Integer) i).getEmployee().getCode())) {
+                report.add(findByCode(i));
+            }
+        }
 
+        return report;
+    }
+
+    // 日報保存
+    @Transactional
+    public ErrorKinds save(Report report, UserDetail userDetail) {
+
+        //ログインユーザーの日報をすべて取得
+        List<Report> loginUserReport = new ArrayList<>();
+        loginUserReport = findByUser(userDetail, (int) findAll().size());
+
+        // 日付重複チェック
+        //　ログインユーザーの日報の日付と登録しようとしている日報の日付が重複した場合エラーとする
+        for (int i = 0; i < (int) loginUserReport.size() ; i++) {
+            if (loginUserReport.get(i).getReportDate().equals(report.getReportDate())) {
+                return ErrorKinds.DATECHECK_ERROR;
+            }
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        report.setCreatedAt(now);
+        report.setUpdatedAt(now);
+        report.setEmployee(userDetail.getEmployee());
+
+        reportRepository.save(report);
+
+        return ErrorKinds.SUCCESS;
+
+    }
 
 }
