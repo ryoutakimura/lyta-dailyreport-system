e2a0739b8dfcc2faae713629994baa98a3cb2ac9
ryoutakimura <yellow11053@gmail.com>
2024-02-20 23:03:05 +0900

    日報詳細画面を実装

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -1,6 +1,5 @@
 package com.techacademy.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -86,6 +86,14 @@ public class ReportController {
         }
 
         return "redirect:/reports";
+    }
+
+    // 日報詳細画面
+    @GetMapping(value = "/{id}/")
+    public String detail(@PathVariable(required = false) Integer id, Model model) {
+
+        model.addAttribute("report", reportService.findByCode(id));
+        return "reports/detail";
     }
 
 }
